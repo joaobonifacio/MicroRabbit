74747fb479326b7f41042cfce96aae55ba6303ee
João Bonifácio <NB27113@novabase.pt>
2023-10-03 17:49:10 +0100

    domain core completo

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeSatmp { set; protected get; }
+
+        protected Event()
+        {
+            TimeSatmp = DateTime.Now;
+        }
+
+
+    }
+}
