33419902f689ae3ec4d4007b1ebc04591e7cea6f
João Bonifácio <NB27113@novabase.pt>
2023-10-03 21:04:46 +0100

    Initial commit of Infrastructure Dependency Project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        //Registar os serviços
+        public static void RegisterConainer(IServiceCollection services) 
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
